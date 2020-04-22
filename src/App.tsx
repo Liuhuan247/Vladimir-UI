198680715ae638938105e581eb3829fe54550201
刘焕 <liuhuan247_2014@163.com>
2020-04-22 12:02:02 +0800

    delete test code

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,6 @@ function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <h1>Hello world</h1>
-                <h2>Hello world</h2>
-                <h3>Hello world</h3>
-                <hr />
-                <code>let a = 'b';</code>
                 <p>
                     Edit <code>src/App.tsx</code> and save to reload.
                 </p>

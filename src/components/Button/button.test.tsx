75496fc56a9645265885a1b6824764ef6e968b1a
刘焕 <liuhuan247_2014@163.com>
2020-04-23 18:36:04 +0800

    button base test

--- src/components/Button/button.test.tsx
+++ src/components/Button/button.test.tsx
@@ -2,8 +2,17 @@ import React from "react";
 import { render } from "@testing-library/react";
 import Button from "./button";
 
-test("my first react test", () => {
-    const wrapper = render(<Button>大兄弟</Button>);
-    const element = wrapper.queryByText("大兄弟");
-    expect(element).toBeTruthy();
+describe("test button component", () => {
+    it("should render the correct button by default props", () => {
+        const wrapper = render(<Button>大兄弟</Button>);
+        const element = wrapper.getByText("大兄弟");
+        expect(element).toBeInTheDocument();
+        expect(element.tagName).toEqual("BUTTON");
+        expect(element).toHaveClass("btn btn-default");
+    });
+
+    it("should render the correct button based on different props", () => {});
+    it("should render a link when btnType equal link and href is provided", () => {});
+
+    it("should render disabled button when disabled set be true", () => {});
 });
